fba2d6b7dfdc26a25fdc1596bfe120270dd4cd0d
Henare Degan <henare.degan@gmail.com>
2013-03-05 15:43:06 +1100

    Dispatcher is no longer required

--- lib/gettext_setup.rb
+++ lib/gettext_setup.rb
@@ -1,7 +1,6 @@
 # Add a callback - to be executed before each request in development,
 # and at startup in production - to plug in theme locale strings.
-require 'dispatcher'
-Dispatcher.to_prepare do
+Rails.configuration.to_prepare do
     repos = [
         FastGettext::TranslationRepository.build('app', :path=>File.join(File.dirname(__FILE__), '..', 'locale-theme'), :type => :po),
         FastGettext::TranslationRepository.build('app', :path=>'locale', :type => :po)
@@ -9,6 +8,3 @@ Dispatcher.to_prepare do
     FastGettext.add_text_domain 'app', :type=>:chain, :chain=>repos
     FastGettext.default_text_domain = 'app'
 end
-
-
-
